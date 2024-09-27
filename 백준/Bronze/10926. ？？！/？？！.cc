a7288e73fa724021e0b63aa34701933dee116f08
dhp308 <143947791+dhp308@users.noreply.github.com>
2024-09-27 11:01:21 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    
+    string in;
+    cin >> in;
+    
+    cout << in << "??!" << endl;
+    
+    return 0;
+    
+    
+}
