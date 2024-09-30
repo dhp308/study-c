e2bb3ce2b7a7b2deae9ca1f1448383cd953d1f44
dhp308 <143947791+dhp308@users.noreply.github.com>
2024-09-30 23:32:37 +0900

    [Bronze III] Title: 최소, 최대, Time: 388 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,34 +1,34 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     int N;
     int Max, Min;
-    cin >> N ;
+    cin >> N;
+
     int* num = new int[N];
 
     for (int i = 0; i < N; i++) {
         cin >> num[i];
     }
-    
-    Max =num[0];
+
+    // 초기 최댓값과 최솟값 설정
+    Max = num[0];
     Min = num[0];
 
-    for (int k = 0; k < N; k++) {
-        if (num[k] > Max) {
-            Max = num[k];
+    // 한 번의 반복문에서 최댓값과 최솟값 모두 찾기
+    for (int i = 1; i < N; i++) {
+        if (num[i] > Max) {
+            Max = num[i];
         }
-    }
-
-    for (int j = 0; j < N; j++) {
-        if (num[j] < Min) {
-            Min = num[j];
+        if (num[i] < Min) {
+            Min = num[i];
         }
     }
 
     cout << Min << " " << Max << endl;
-    
-    delete[] num;
+
+    delete[] num;  // 동적 메모리 해제
 
     return 0;
-
 }
