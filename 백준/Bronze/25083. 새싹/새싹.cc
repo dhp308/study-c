74d312c326f72c274b30aad842d1e2277b72e445
dhp308 <143947791+dhp308@users.noreply.github.com>
2025-10-17 03:22:52 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+
+	cout << "         ,r'\"7" << endl
+	<< "r`-_   ,'  ,/" << endl
+	<< " \\. \". L_r'" << endl
+	<< "   `~\\/" << endl
+	<< "      |" << endl
+	<< "      |" << endl;
+
+	 
+		          
+	return 0;
+	
+}
